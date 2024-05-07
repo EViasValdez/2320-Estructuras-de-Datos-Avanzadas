6cf41c3c5b164ea1fac1b89e959ead74d90a3780
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-05-07 10:19:29 -0600

    Add files via upload

--- TablasHash/TablasHash/obj/Debug/net6.0/TablasHash.AssemblyInfo.cs
+++ TablasHash/TablasHash/obj/Debug/net6.0/TablasHash.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TablasHash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+816646904a1ea4f49bf53da831a4434922ddf250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29181b845143dacd668cbaa012553dfe3d060053")]
 [assembly: System.Reflection.AssemblyProductAttribute("TablasHash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TablasHash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
